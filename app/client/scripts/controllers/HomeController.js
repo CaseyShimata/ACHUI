da2d1b6e5812d942665f8218bd6ca3a1597dabc7
casey shimata <caseyshimata@10MISDEV7096.local>
2022-07-04 22:59:31 -0600

    the json is available to the front end as raw json. Needs to be mapped to JS objects to be more easily consumed.

--- app/client/scripts/controllers/HomeController.js
+++ app/client/scripts/controllers/HomeController.js
@@ -7,11 +7,11 @@ myApp.controller('HomeController', function ($scope, fileUploadService) {
             promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
 
         promise.then(function (response) {
+            //todo: map json to JS objects to be more easily consumed on the front end.
             $scope.serverResponse = response;
             debugger;
         }, function () {
             $scope.serverResponse = 'An error has occurred';
         })
     };
-
 });
